94a9fa6c5414b80f5eb9e156a709971bb87c3088
kennyoz <ihorkenny@yandex.ru>
2024-11-21 20:47:56 +0300

    add new tests

--- pages/login.py
+++ pages/login.py
@@ -11,27 +11,27 @@ import time
 class Login_input(BasePage):
     #локаторы
     button_login_input = (By.XPATH,"//*[@class='wrap_icon inner-table-block1 person']")         #кнопка логина на главной странице
-    placeholder_login = (By.XPATH,"//*[@class='popup-singin']")
-    input_login_in_placeholder = (By.XPATH,"//*[@id='USER_LOGIN_POPUP']")
-    input_password_in_placeholder = (By.XPATH,"//*[@id='USER_PASSWORD_POPUP']")
-    block_error = (By.XPATH,"//*[@class='filter block error']")
-    login_button_in_placeholder = (By.XPATH,"//*[@data-type='phone']")
+    placeholder_login = (By.XPATH,"//*[@class='popup-singin']")                                 # локатор плейхолдера лк
+    input_login_in_placeholder = (By.XPATH,"//*[@id='USER_LOGIN_POPUP']")                       # поле инпут логин
+    input_password_in_placeholder = (By.XPATH,"//*[@id='USER_PASSWORD_POPUP']")                 # поле инпут пароль
+    block_error = (By.XPATH,"//*[@class='filter block error']")                                 # чек ошибки при неверном заполнении
+    login_button_in_placeholder = (By.XPATH,"//*[@data-type='phone']")                          # кнопка входа с заполненым логином и паролем
 
 
-    def find_and_click_login(self):
+    def find_and_click_login(self):                                                                #клик на кнопку входа в личный кабинет
         self.is_element_visible(Login_input.button_login_input)
         self.find_element(Login_input.button_login_input).click()
         self.is_element_visible(Login_input.placeholder_login)
 
 
-    def enter_valid_name_and_login(self):
+    def enter_valid_name_and_login(self):                                                                   #
         self.find_element(Login_input.input_login_in_placeholder).send_keys(Constants.LOGIN)
         self.find_element(Login_input.input_password_in_placeholder).send_keys(Constants.PASSWORD)
         self.find_element(Login_input.login_button_in_placeholder).click()
         self.is_element_visible(Login_input.block_error)
 
 
-    def enter_invalid_name_and_valid_password(self):
+    def enter_invalid_name_and_valid_password(self):                                                        #
         self.find_element(Login_input.input_login_in_placeholder).send_keys(Constants.INVALID_LOGIN)
         self.find_element(Login_input.input_password_in_placeholder).send_keys(Constants.PASSWORD)
         self.find_element(Login_input.login_button_in_placeholder).click()
@@ -45,3 +45,9 @@ class Login_input(BasePage):
         self.find_element(Login_input.login_button_in_placeholder).click()
         self.is_element_visible(Login_input.block_error)
 
+
+    def enter_invalid_name_invalid_password(self):
+        self.find_element(Login_input.input_login_in_placeholder).send_keys(Constants.INVALID_LOGIN)
+        self.find_element(Login_input.input_password_in_placeholder).send_keys(Constants.INVALID_PASSWORD)
+        self.find_element(Login_input.login_button_in_placeholder).click()
+        self.is_element_visible(Login_input.block_error)
--- tests/test_login.py
+++ tests/test_login.py
@@ -6,29 +6,36 @@ from pages import Constants
 
 
 @pytest.mark.smoke
-def test_valid_login_input(driver):
+def test_valid_login_input(driver):                                             #клик на кнопку личный кабинети проверка видимости плейсхолдера
     driver.get(Constants.URL)
     login = Login_input(driver)
     login.find_and_click_login()
 
 @pytest.mark.smoke
-def test_input_value_name_and_password(driver):
+def test_input_value_name_and_password(driver):                                 #Ввод валидного логина и пароля
     driver.get(Constants.URL)
     login = Login_input(driver)
     login.find_and_click_login()
     login.enter_valid_name_and_login()
 
 @pytest.mark.smoke
-def test_input_valid_name_and_invalid_password(driver):
+def test_input_valid_name_and_invalid_password(driver):                          #Ввод валидного логина и невалидного пароля
     driver.get(Constants.URL)
     login = Login_input(driver)
     login.find_and_click_login()
     login.enter_invalid_name_and_valid_password()
 
 @pytest.mark.smoke
-def test_input_invalid_name_and_valid_password(driver):
+def test_input_invalid_name_and_valid_password(driver):                             #Ввод невалидного логина и валидного пароля
     driver.get(Constants.URL)
     login = Login_input(driver)
     login.find_and_click_login()
-    login.enter_invalid_name_and_valid_password()
+    login.enter_name_and_invalid_password()
+
+@pytest.mark.smoke
+def test_input_invalid_name_and_invalid_password(driver):                           #ввод невалидного имени и невалидного пароля
+    driver.get(Constants.URL)
+    login = Login_input(driver)
+    login.find_and_click_login()
+    login.enter_invalid_name_invalid_password()
 
